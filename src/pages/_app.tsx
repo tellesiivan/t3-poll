86e7866d76b87e539f910b9d1ab9c15eb697bc91
Ivan Telles <tellezivan7@gmail.com>
2023-04-04 21:10:02 -0700

    nextui theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,27 @@
 import { type AppType } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 import { api } from "t3-poll/utils/api";
 
 import "t3-poll/styles/globals.css";
+import { theme } from "t3-poll/theme/theme";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
+      <NextThemesProvider
+        defaultTheme="system"
+        attribute="class"
+        value={{
+          light: theme.className,
+        }}
+      >
+        <NextUIProvider theme={theme}>
+          <Component {...pageProps} />
+        </NextUIProvider>
+      </NextThemesProvider>
     </ClerkProvider>
   );
 };
